176a16b9ff758b4baf59066e81761f189348c019
João Pedro <contato.joaopedroprogram@gmail.com>
2025-11-14 19:08:13 -0300

    comitando

--- listas/DefesacomTorres.py
+++ listas/DefesacomTorres.py
@@ -6,12 +6,22 @@ def simularTiro(linhaInicial, colInicial, direcao, tabuleiro):
         1,0
     )
     esquerda = (
-        0,1
+        0,-1
     )
     direita = (
-        1,0
+        0,1
     )
-        
+    if direcao == 1:
+        direcao = cima
+    elif direcao == 2:
+        direcao = baixo
+    elif direcao == 3:
+        direcao = esquerda
+    elif direcao == 4:
+        direcao = direita
+    linhaAtual = linhaInicial + direcao[0]
+    colAtual = colInicial + direcao[1]
+    
 while True:
     numLinha = int(input('Digite o o número de linhas:'))
     numCol = int(input('Digite o número de colunas: '))
