4b18a24085095b54b2c51fb16225f91b497a3627
João Pedro <contato.joaopedroprogram@gmail.com>
2025-11-14 18:44:47 -0300

    realizando função e criação do projeto

--- listas/DefesacomTorres.py
+++ listas/DefesacomTorres.py
@@ -1,16 +1,28 @@
+def simularTiro(linhaInicial, colInicial, direcao, tabuleiro):
+    cima = (
+        -1, 0
+    )
+    baixo = (
+        1,0
+    )
+    esquerda = (
+        0,1
+    )
+    direita = (
+        1,0
+    )
+        
 while True:
-    r = int(input('Digite o o número de linhas:'))
-    s = int(input('Digite o número de colunas: '))
-    numLadrao=int(input('Digite o número de ladrões: '))
-    if r <= 1 or s <= 1 or numLadrao < 1:
+    numLinha = int(input('Digite o o número de linhas:'))
+    numCol = int(input('Digite o número de colunas: '))
+    if numLinha <= 1 or numCol <= 1:
         print('Número inválido! Tente novamente.')
     else:
         break
-matriz = []
-valor = ['.', 'T', 'n']
-for i in range(r):
+tabuleiro = []
+for i in range(numLinha):
     linha = []
-    for j in range(s):
-        linha.append(0)
-    matriz.append(linha)
-    
+    for j in range(numCol):
+        linha.append('.')
+    tabuleiro.append(linha)
+print(tabuleiro)
